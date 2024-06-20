88ccdc43fffe26465da160d639556fcee4c75918
Ajay-Babare <ajaybabare97@gmail.com>
2024-06-20 15:02:09 +0530

    Composite Pattern

--- src/VerticalFlipped.java
+++ src/VerticalFlipped.java
@@ -7,15 +7,37 @@ public class VerticalFlipped {
 		System.out.println("Enter n : ");
 		int n = sc.nextInt();
 		
+//		for(int i=1;i<=n;i++) {
+//			for(int j=1;j<=n;j++) {
+//				if((i+j)>n)
+//					System.out.print("*"+" ");
+//				else
+//					System.out.print(" "+" ");
+//			}
+//			System.out.println();
+//		}
+		
 		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n;j++) {
-				if((i+j)>n)
-					System.out.print("*"+" ");
-				else
-					System.out.print(" "+" ");
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
 			}
 			System.out.println();
 		}
+		
+		System.out.println("***************");
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print((char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
